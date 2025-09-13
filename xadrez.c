f257bbae85a6a80d2eafa95d6997ff3efea78564
Felipe Alcantara e Silva <fas_1986@hotmail.com>
2025-09-13 15:38:29 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -2,12 +2,13 @@
 
 int main(){
 
-int peca, t = 1, b = 1, r;
+int peca, t = 1, b = 1, r, movimentocavalo = 1;
 
 printf("Digite a peça a ser movimentada:\n"); // Entrada do usuário;
 printf("1. TORRE - 5 casas para a direita\n");
 printf("2. BISPO - 5 casas na diagonal\n");
 printf("3. RAINHA - 8 casas para a esquerda\n");
+printf("4. CAVALO - 2 casas para baixo e 1 casa para a esquerda\n");
 scanf("%d", &peca);
 
     switch (peca)
@@ -36,6 +37,16 @@ scanf("%d", &peca);
         case 3: // Movimentação da Rainha;
         for (int r = 1; r <= 8; r++){
             printf("Esquerda\n");
+        }
+        break;
+
+        case 4: // Movimentação do Cavalo
+        while(movimentocavalo--)
+        {
+            for(int i = 0; i < 2; i++){
+                printf("Baixo\n");
+            }
+                printf("Esquerda\n");
         }
         break;
 
